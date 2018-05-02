532e77251a5f06f22a4d2cc46948e85fc5107aee
Anurag <Anuraggauutam77@gmail.com>
2018-05-02 15:36:43 +0530

    Changes in DB

--- config/configuration.js
+++ config/configuration.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-db: 'mongodb://mystuffuser:mystuffuser123@ds225038.mlab.com:25038/mystuff',
+db: 'mongodb://pushnotification:pushnotification@ds111050.mlab.com:11050/pushnotification',
  //db_dev: 'mongodb://localhost:27017/pushnotification'
  db_dev:'mongodb://pushnotification:pushnotification@ds111050.mlab.com:11050/pushnotification'
 
