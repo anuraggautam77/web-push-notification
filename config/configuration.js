fb5511c79da8872af5446eae9187574bc36e41a2
Anurag <Anuraggauutam77@gmail.com>
2018-06-23 23:18:12 +0530

    changes in database

--- config/configuration.js
+++ config/configuration.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 db: 'mongodb://pushnotification:pushnotification@ds111050.mlab.com:11050/pushnotification',
-db_dev: 'mongodb://localhost:27017/pushnotification'
-//db_dev:'mongodb://pushnotification:pushnotification@ds111050.mlab.com:11050/pushnotification'
+//db_dev: 'mongodb://localhost:27017/pushnotification'
+db_dev:'mongodb://pushnotification:pushnotification@ds111050.mlab.com:11050/pushnotification'
 
 };
