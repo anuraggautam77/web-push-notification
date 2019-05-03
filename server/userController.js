c56d7fb11d0f9df6e60abe6f7f87e77d46f585ef
Anurag G <anuraggautam77@gmail.com>
2019-05-03 13:16:16 +0530

    Update userController.js
    Updated fcm details

--- server/userController.js
+++ server/userController.js
@@ -103,7 +103,7 @@ module.exports = class UserController {
                         method: 'POST',
                         headers: {
                             'Content-Type': ' application/json',
-                            'Authorization': 'key=AIzaSyDwbkJd-lz_OLS8Vb62wU3kv55wV500MA8'
+                            'Authorization': 'AIzaSyDTq0EuIQFLW2e3-a_unMfI7Bl2iTpjemg'
                         },
                         body: JSON.stringify(
                                 {
@@ -163,7 +163,7 @@ module.exports = class UserController {
                             method: 'POST',
                             headers: {
                                 'Content-Type': ' application/json',
-                                'Authorization': 'key=AIzaSyDwbkJd-lz_OLS8Vb62wU3kv55wV500MA8'
+                                'Authorization': 'AIzaSyDTq0EuIQFLW2e3-a_unMfI7Bl2iTpjemg'
                             },
                             body: JSON.stringify(
                                     {
